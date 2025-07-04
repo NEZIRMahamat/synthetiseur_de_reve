cca9226e972628433d5b48bcc56c36bda3e5b971
NEZIRMahamat <95191142+NEZIRMahamat@users.noreply.github.com>
2025-07-04 19:07:21 +0200

    Structuration du backend

--- backend.py
+++ backend.py
@@ -22,8 +22,10 @@ def read_file(file_path):
 # Trancription de l'audio en text : Audio to text -> print(result)
 def speech_to_text(file_path, language="fr"):
     # Le cas où le fichier n'existe pas
-    
-    client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+    if not os.path.exists(file):
+        raise FileNotFoundError(f"Le fichier introuuvable {file_path}")
+
+    client = Groq(api_key=os.environ["GROQ_API_KEY"])
 
     # Open the audio file
     with open(file_path, "rb") as file:
@@ -31,64 +33,70 @@ def speech_to_text(file_path, language="fr"):
         transcription = client.audio.transcriptions.create(
         file=file, # Required audio file
         model="whisper-large-v3-turbo", # Required model to use for transcription
-        prompt="Extrait de l'audio le texte, de la façon la plus consise et la plus factuelle que tu peux",  # Optional
+        prompt="Extrait de l'audio le texte, de la façon la plus consise et factuelle que tu peux",  # Optional
         response_format="verbose_json",  # Optional
         timestamp_granularities = ["word", "segment"], # Optional (must set response_format to "json" to use and can specify "word", "segment" (default), or both)
         language=language,  # Optional
-        temperature=0.5 # Optional
+        temperature=0.5 # Optional # 50% factuelle et 50% créative
     )
 
     return transcription.text
 
-# Normalisation des resultats des proba à ce la somme des caractériistiqus de l'utilisateur vaut 1 (softmax)
+# Normalisation des resultats des proba à ce que la somme des proba des caractéristiques de l'utilisateur soit 1 (softmax)
 def normalize_sentiment(sentiments):
-    sentiments_normalized = {}
     exp_values = {k: math.exp(v) for k, v in sentiments.items() if isinstance(v, (int, float))}
-    total = sum(exp_values.values())
-    for key, value in exp_values.items():
-        sentiments_normalized[key] = value / total if total != 0 else 0
     
-    return sentiments_normalized
+    total = sum(exp_values.values()) or 1e-10 # La somme des expo
+
+    return {key: (value/total) for key, value in exp_values.items()}
 
 # Classification du rêve
 def classifier_reve(text):
-    api_key = os.environ.get("MISTRAL_API_KEY")
-    model_0 = "mistral-small-latest"
-    model = "mistral-large-latest"
+    api_key = os.environ["MISTRAL_API_KEY"]
+    mistral_model = "mistral-small-latest"
     client = Mistral(api_key=api_key)
     messages = [
         {
             "role": "system",
             "content": read_file("./context.txt")
         },
-
         {
             "role": "user",
             "content": f"Analyse le texte suivant et envoie ta reponse uniquement au format JSON : {text}",
         }
     ]
-    chat_response = client.chat.complete(
-        model = model_0,
-        messages = messages,
-        response_format = {
-            "type": "json_object",
+    try:
+        chat_response = client.chat.complete(
+            model = mistral_model,
+            messages = messages,
+            response_format = {
+                "type": "json_object",
+            }
+        )
+        result = json.loads(chat_response.choices[0].message.content)
+        
+        return normalize_sentiment(result)
+    except Exception as e:
+        print(f"Erreur API Mistral : {e}")
+        return {
+            "triste": 0.0,
+            "heureux": 0.0,
+            "en_colere": 0.0,
+            "fatigue": 0.0,
+            "anxieux": 0.0,
+            "neutre": 0.0
         }
-    )
-    result = json.loads(chat_response.choices[0].message.content)
-    
-    return normalize_sentiment(result)
 
 
 # Prompt to image -> display(image, video)
 def prompt_to_image(prompt):
     url = 'https://clipdrop-api.co/text-to-image/v1'
-    headers = { 'x-api-key': os.environ.get("CLIPDROP_API_KEY")}
+    headers = { 'x-api-key': os.environ["CLIPDROP_API_KEY"]}
     files = {
         'prompt': (None, f"Génère moi ce rêve text ci-dessous en image sympa, immersive et factuelle: {prompt}", 'text/plain')
     }
-   
-    response = requests.post(url=url, headers=headers, files=files)
     try:
+        response = requests.post(url=url, headers=headers, files=files)
         if (response.ok):
             # Optionally save the image to a file
             time_temp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -96,13 +104,13 @@ def prompt_to_image(prompt):
             
             os.makedirs("images", exist_ok=True)
             with open(f"images/{filename}", 'wb') as img_file:
-                 img_file.write(response.content)
-            return response.headers   # contains the bytes of the returned image
+                img_file.write(response.content)
+            return response.headers   # heasers content the consumed & renaiming credits API Key
 
         else:
             response.raise_for_status()
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Erreur API Clipdrop: {e}")
         return None
 
 
